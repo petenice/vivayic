a2391a71743cffc244806d6f20f8323e35fa1348
petenice <peter@petenice.com>
2018-08-02 17:33:25 -0700

    done

--- web/js/functions.js
+++ web/js/functions.js
@@ -9,3 +9,19 @@ $('.nav-close').on('click', function(){
     $(".button a").toggleClass('btn-open').toggleClass('btn-close');
     open = false;
 });
+
+
+$(document).ready(function () {
+    $('.thumb-tog-up').click(function (e) {
+        e.preventDefault();
+        $('.lnk-up').attr('src', function (i, oldSrc) {
+            return oldSrc == '../imgz/icon-thumb-up-off.png' ? '../imgz/icon-thumb-up-off.png' : '../imgz/icon-thumb-up-on.png';
+        });
+    });
+    $('.thumb-tog-down').click(function (e) {
+        e.preventDefault();
+        $('.lnk-down').attr('src', function (i, oldSrc) {
+            return oldSrc == '../imgz/icon-thumb-down-off.png' ? '../imgz/icon-thumb-down-off.png' : '../imgz/icon-thumb-down-on.png';
+        });
+    });
+});
