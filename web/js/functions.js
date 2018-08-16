f730c817a43fbe378f7d4b10861714f83a97de85
petenice <peter@petenice.com>
2018-08-15 17:50:39 -0700

    done

--- web/js/functions.js
+++ web/js/functions.js
@@ -25,3 +25,26 @@ $(document).ready(function () {
         });
     });
 });
+
+
+  // $(function () { // wait for document ready
+  //   // init
+  //   var controller = new ScrollMagic.Controller({
+  //     globalSceneOptions: {
+  //       triggerHook: 'onLeave'
+  //     }
+  //   });
+  //
+  //   // get all slides
+  //   var slides = document.querySelectorAll("#hero-feature");
+  //
+  //   // create scene for every slide
+  //   for (var i=0; i<slides.length; i++) {
+  //     new ScrollMagic.Scene({
+  //         triggerElement: slides[i]
+  //       })
+  //       .setPin(slides[i])
+  //       // .addIndicators() // add indicators (requires plugin)
+  //       .addTo(controller);
+  //   }
+  // });
